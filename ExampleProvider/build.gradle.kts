24d6447c30a3ea5ef7f14c61f8f9ff76b67041da
Spyso <angy260819@gmail.com>
2026-04-18 10:20:13 +0200

    Modify cloudstream properties and restore dependencies
    Updated cloudstream properties with new description, authors, and language. Added dependencies section back.

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,32 +1,11 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-}
-
-// Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
+    description = "Mon extension personnelle"
+    authors = listOf("Spyro")
+    status = 1
     tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
+    language = "fr"
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
 }
 
@@ -35,4 +14,9 @@ android {
         buildConfig = true
         viewBinding = true
     }
+}
+
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
